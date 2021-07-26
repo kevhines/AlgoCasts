071eb8f015cd73616a3c22174a62f441b9e6f8b0
Kevin Hines <kevhines@gmail.com>
2021-07-26 17:18:22 -0400

    solution pyramid2

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,17 +14,39 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-    let noSpace = ""
-    let withSpace = ""
-for (i=1;i <= n; i++) {
-    noSpace = (i > 1) ? noSpace + "##" : "#"
-    withSpace = noSpace
-    for (j=1 + i; j <= n; j++) {
-        withSpace = " " + withSpace + " "
-    }
-    console.log(withSpace)
+function pyramid(n, row = 0, step = "") {
+if (n === row) {
+    return
 }
+
+if (step.length === n * 2 -1) {
+    console.log(step)
+    return pyramid(n,row + 1)
+}
+
+if (step.length === 0) {
+    step = "#"
+} else if (step.length < (row + 1) * 2 - 1) {
+    step = step + "##"
+} else {
+    step = " " + step + " "
+}
+
+pyramid(n,row,step)
+
 }
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//     let noSpace = ""
+//     let withSpace = ""
+// for (i=1;i <= n; i++) {
+//     noSpace = (i > 1) ? noSpace + "##" : "#"
+//     withSpace = noSpace
+//     for (j=1 + i; j <= n; j++) {
+//         withSpace = " " + withSpace + " "
+//     }
+//     console.log(withSpace)
+// }
+// }
