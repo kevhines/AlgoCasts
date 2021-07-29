792fa0e4de4e5612bffb6ccb099b2f1fa3012c23
Kevin Hines <kevhines@gmail.com>
2021-07-29 13:41:03 -0400

    solution fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,28 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fibStore(func) {
+    const cache = {};
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args]
+        }
+
+        const result = func.apply(this, args)
+        cache[args] = result
+        return result
+    }
+}
+
+function originalFib(n) {
+    if ( n === 0 || n === 1) {
+        return n
+    } else {
+        return fib(n-1) + fib(n - 2)
+    }
+
+}
+
+const fib = fibStore(originalFib)
 
 module.exports = fib;
